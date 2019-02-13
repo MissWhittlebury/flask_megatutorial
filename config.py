fecd3e062c5652c2e29d0e55e54f83e577669540
Daniel Wojtak <danee628@gmail.com>
2019-02-13 10:43:27 -0600

    chapter 7

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
                                         'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 25))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    ##TODO(dwojtak): This would be actual email address if you actually want
+    # to host app
+    ADMINS = ['unneeded@example.com']
