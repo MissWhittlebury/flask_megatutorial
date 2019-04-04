64b919de80d36b428c1b030a83e262283f77e651
Daniel Wojtak <danee628@gmail.com>
2019-04-04 17:06:28 -0500

    Chapter 10 Resetting a User Password Not Tested

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.email import send_password_reset_email
-from app.forms import EditProfileForm, LoginForm, PostForm, RegistrationForm, ResetPasswordRequestForm
+from app.forms import EditProfileForm, LoginForm, PostForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import Post, User
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
@@ -168,3 +168,22 @@ def reset_password_request():
         return redirect(url_for('login'))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
+
+    
+        
